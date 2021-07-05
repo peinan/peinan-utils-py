d69ed724e5ebef5ac042012c992e104daa30f4ad
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2021-07-05 18:17:13 +0900

    added logger

--- peinan_utils/__init__.py
+++ peinan_utils/__init__.py
@@ -4,4 +4,5 @@ from .parser import Parser
 from .vectorizer import Vectorizer
 from .stat_utils import Statist
 from .matplotlib_utils import init_matplotlib
+from .logger import get_logger
 init_matplotlib()

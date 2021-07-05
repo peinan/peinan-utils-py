d090aa78e955ee5688e483ea68307cdf89955f98
Peinan ZHANG <zhang_peinan@cyberagent.co.jp>
2021-07-05 18:17:34 +0900

    updated matplotlib initializer

--- peinan_utils/matplotlib_utils.py
+++ peinan_utils/matplotlib_utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import matplotlib
 from matplotlib import font_manager
+from distutils.version import LooseVersion
 
 
 def init_matplotlib(weight='medium'):
@@ -23,7 +24,12 @@ def init_matplotlib(weight='medium'):
     font_dir_path = Path(__file__).parent.resolve() / Path(FONTS_DIR)
     font_dirs = [font_dir_path, ]
     font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
-    font_list = font_manager.createFontList(font_files)
-    font_manager.fontManager.ttflist.extend(font_list)
 
+    is_support_createFontList = LooseVersion(matplotlib.__version__) < '3.2'
+    if is_support_createFontList:
+        font_list = font_manager.createFontList(font_files)
+        font_manager.fontManager.ttflist.extend(font_list)
+    else:
+        for fpath in font_files:
+            font_manager.fontManager.addfont(fpath)
     matplotlib.rc('font', **font_settings)
